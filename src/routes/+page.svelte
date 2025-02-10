329d8b883ade8d67d5b805c0e6a1f653094ee537
Mathieu TUDISCO <oss@mathieutu.dev>
2025-02-10 13:28:59 +0100

    🎉 SvelteKit install.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Alert } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and <span class="text-red-400">Fooo</span>try submitting again.
+  </Alert>
+</div>
