b73117eb964c21934df8459aff8e4eb325bf8cbb
Mathieu TUDISCO <oss@mathieutu.dev>
2025-03-08 19:56:33 +0100

    🎨 Prettier.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import prettier from 'eslint-config-prettier';
-import js from '@eslint/js';
-import { includeIgnoreFile } from '@eslint/compat';
-import svelte from 'eslint-plugin-svelte';
-import globals from 'globals';
-import { fileURLToPath } from 'node:url';
-import ts from 'typescript-eslint';
+import prettier from 'eslint-config-prettier'
+import js from '@eslint/js'
+import { includeIgnoreFile } from '@eslint/compat'
+import svelte from 'eslint-plugin-svelte'
+import globals from 'globals'
+import { fileURLToPath } from 'node:url'
+import ts from 'typescript-eslint'
 
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url))
 
 export default ts.config(
 	includeIgnoreFile(gitignorePath),
@@ -37,4 +37,4 @@ export default ts.config(
 			},
 		},
 	}
-);
+)
