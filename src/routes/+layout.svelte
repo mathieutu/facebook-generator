76a2627d415c1b07cc11224754cbfcefbbcfcee4
Mathieu TUDISCO <oss@mathieutu.dev>
2025-03-08 19:51:50 +0100

    💄 Ensure full page background.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	let { children } = $props()
 </script>
 
-<main class="not-print:bg-gray-50 not-print:p-6">
+<main class="not-print:min-h-screen not-print:bg-gray-50 not-print:p-6">
 	<div class="mx-auto w-[210mm] rounded-lg bg-white not-print:p-6">
 		{@render children()}
 	</div>
